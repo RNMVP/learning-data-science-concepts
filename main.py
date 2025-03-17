0c3768bc85f9fdd90670a3b09f8f503ebea31175
RenanNunesViana <renan.devops.viana@gmail.com>
2025-03-17 12:01:42 -0300

    desenvolvendo analises mais detalhadas, filtrando por valores das colunas. outros dados foram inseridos no projeto.

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 notas = pd.read_csv("data/ratings.csv")
 print(notas)
 
 notas['rating'].describe()
 
-notas['rating'].plot(kind='hist')
+notas.columns = ['IdUsuario', 'IdFilme', 'Nota', 'Hora']
 
-sns.boxplot(notas['rating'])
+print(notas.head())
+
+notas['Nota'].plot(kind='hist')
+plt.show()
+
+sns.boxplot(notas['Nota'])
+plt.show()
+
+
+#%%
+filmes = pd.read_csv("data/movies.csv")
+
+print(filmes.columns)
+filmes.columns = ['IdFilme', 'Titulo', 'Genero']
+print(filmes.head())
+
+
+
+#%%
+mediaFilme2 = notas.query('IdFilme==2')['Nota'].mean()
+print(mediaFilme2)
+#%%
+medias = notas.groupby('IdFilme')['Nota'].mean()
+print(medias)
+#%%
+medias.plot(kind='hist')
+plt.show()
